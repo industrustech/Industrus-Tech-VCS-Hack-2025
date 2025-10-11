bf6bcb6238c57d752e7e5b38cb5a0d6f1a8f0e8c
Elwin Augustin <37208255+elwinaugustin@users.noreply.github.com>
2025-10-11 08:32:49 +0530

    Create shortcut_launcher.bat
    with limited process, i just shortlisted & created microsoft based apps shortcuts launcher,whill pulls out every app in at a time,not useful for everyone

---
+++ pr1/shortcut_launcher.bat
@@ -0,0 +1,14 @@
+@echo off
+:: Launches commonly used Microsoft and Windows apps by process name
+
+echo Launching favorite apps...
+
+start msedge       :: Microsoft Edge browser
+start winword      :: Microsoft Word
+start excel        :: Microsoft Excel
+start powerpnt     :: Microsoft PowerPoint
+start mspaint      :: Windows Paint
+start notepad      :: Notepad
+
+echo All apps launched!
+pause
